6a2e16035a2cbd2e1ebcc3117591e0e0cf8e1e36
Krystian <40540225+KrysOlechowski@users.noreply.github.com>
2020-11-01 11:03:02 +0100

    adding /test endpoint

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/KrysOlechowski/heroku1
 
 go 1.15
+
+require rsc.io/quote v1.5.2
--- hello.go
+++ hello.go
@@ -4,17 +4,27 @@ import (
 	"io"
 	"log"
 	"net/http"
-	"os"
+
+	"rsc.io/quote"
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "HELLO WORLD")
 }
 
+func test(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "HELLO TEST")
+}
+
+func hello2() string {
+	return quote.Hello()
+}
+
 func main() {
-	port := os.Getenv("PORT")
-	// port := "3000"
+	// port := os.Getenv("PORT")
+	port := "3000"
 	http.HandleFunc("/", hello)
+	http.HandleFunc("/test", test)
 	log.Print("Listening on :" + port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
