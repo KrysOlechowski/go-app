a16238e8f7c6b91ded8a59d7cb88129cb467d72b
Krystian <40540225+KrysOlechowski@users.noreply.github.com>
2020-11-03 20:11:49 +0100

    connect to mongodb

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/KrysOlechowski/heroku1
 
 go 1.15
 
-require rsc.io/quote v1.5.2
+require (
+	github.com/gorilla/mux v1.8.0
+	go.mongodb.org/mongo-driver v1.4.2
+	rsc.io/quote v1.5.2
+)
--- hello.go
+++ hello.go
@@ -4,8 +4,8 @@ import (
 	"io"
 	"log"
 	"net/http"
-	"os"
 
+	"github.com/KrysOlechowski/heroku1/middleware"
 	"rsc.io/quote"
 )
 
@@ -13,19 +13,19 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "HELLO WORLD")
 }
 
-func test(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "HELLO TEST")
-}
+// func test(w http.ResponseWriter, r *http.Request) {
+// 	io.WriteString(w, "HELLO TEST")
+// }
 
 func hello2() string {
 	return quote.Hello()
 }
 
 func main() {
-	port := os.Getenv("PORT")
-	// port := "3000"
+	// port := os.Getenv("PORT")
+	port := "3000"
 	http.HandleFunc("/", hello)
-	http.HandleFunc("/test", test)
+	http.HandleFunc("/test", middleware.GetAllTask)
 	log.Print("Listening on :" + port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
