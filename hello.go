3b6983a450a390b295a76e8bea8270cc4b79b4fc
Krystian <40540225+KrysOlechowski@users.noreply.github.com>
2020-11-03 21:44:15 +0100

    add router

--- hello.go
+++ hello.go
@@ -1,33 +1,34 @@
 package main
 
 import (
-	"io"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
-	// "github.com/KrysOlechowski/heroku1/middleware"
-	"rsc.io/quote"
+	"github.com/KrysOlechowski/heroku1/router"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "HELLO WORLDxxxxxx")
-}
+// func hello(w http.ResponseWriter, r *http.Request) {
+// 	io.WriteString(w, "HELLO WORLDxxxxxx")
+// }
 
-func test(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "HELLO TEST")
-}
+// func test(w http.ResponseWriter, r *http.Request) {
+// 	io.WriteString(w, "HELLO TEST")
+// }
 
-func hello2() string {
-	return quote.Hello()
-}
+// func hello2() string {
+// 	return quote.Hello()
+// }
 
 func main() {
 	port := os.Getenv("PORT")
 	// port := "3000"
-	http.HandleFunc("/", hello)
-	http.HandleFunc("/test", test)
-	log.Print("Listening on :" + port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
-
+	// http.HandleFunc("/", hello)
+	// http.HandleFunc("/test", middleware.GetAllTask)
+	// log.Print("Listening on :" + port)
+	// log.Fatal(http.ListenAndServe(":"+port, nil))
+	r := router.Router()
+	fmt.Println("Starting server on the port 8080...")
+	log.Fatal(http.ListenAndServe(":"+port, r))
 }
