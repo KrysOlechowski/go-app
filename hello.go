11ba5524edc303c32ce12dac43ffcf3663f79482
Krystian <40540225+KrysOlechowski@users.noreply.github.com>
2020-11-03 21:28:08 +0100

    old behavior

--- hello.go
+++ hello.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/KrysOlechowski/heroku1/middleware"
+	// "github.com/KrysOlechowski/heroku1/middleware"
 	"rsc.io/quote"
 )
 
@@ -14,9 +14,9 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "HELLO WORLDxxxxxx")
 }
 
-// func test(w http.ResponseWriter, r *http.Request) {
-// 	io.WriteString(w, "HELLO TEST")
-// }
+func test(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "HELLO TEST")
+}
 
 func hello2() string {
 	return quote.Hello()
@@ -26,7 +26,8 @@ func main() {
 	port := os.Getenv("PORT")
 	// port := "3000"
 	http.HandleFunc("/", hello)
-	http.HandleFunc("/test", middleware.GetAllTask)
+	http.HandleFunc("/test", test)
 	log.Print("Listening on :" + port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
+
 }
