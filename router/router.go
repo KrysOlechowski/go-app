7e8555e5d5631e1eec7106f1500356a890b50409
Krystian <40540225+KrysOlechowski@users.noreply.github.com>
2020-11-04 17:59:31 +0100

    change to /test

--- router/router.go
+++ router/router.go
@@ -10,7 +10,7 @@ func Router() *mux.Router {
 
 	router := mux.NewRouter()
 
-	router.HandleFunc("/api/task", middleware.GetAllTask).Methods("GET", "OPTIONS")
+	router.HandleFunc("/test", middleware.GetAllTask).Methods("GET", "OPTIONS")
 	router.HandleFunc("/api/task", middleware.CreateTask).Methods("POST", "OPTIONS")
 	router.HandleFunc("/api/task/{id}", middleware.TaskComplete).Methods("PUT", "OPTIONS")
 	router.HandleFunc("/api/undoTask/{id}", middleware.UndoTask).Methods("PUT", "OPTIONS")
