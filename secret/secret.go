d1f271c9af2c1909949906e52921f6ecc71759f4
Krystian <40540225+KrysOlechowski@users.noreply.github.com>
2020-11-05 19:56:36 +0100

    delete gitignore

---
+++ secret/secret.go
@@ -0,0 +1,4 @@
+package secret
+
+//MongoDBConnectionString for connect to database
+const MongoDBConnectionString = "mongodb+srv://projecttest:projectpass@cluster0.quzdq.mongodb.net/test?retryWrites=true&w=majority"
